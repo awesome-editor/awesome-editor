575e4739bf834d1cbbaeecbeca61acf2bd7eee76
Uriel Avalos <uri@frankandrobot.com>
2016-01-25 16:51:14 -0600

    added app

---
+++ src/app.js
@@ -0,0 +1,95 @@
+import 'flexboxgrid/css/flexboxgrid.css!'
+import './css/app.css!'
+import './util/kefirBaconJsOnValue'
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import DocStore from './stores/DocStore'
+import Editor from './editor/Editor'
+import Preview from './editor/Preview.js';
+
+//import TagView from 'tags/TagsView'
+//import FilePreview from 'tags/file.preview'
+
+
+
+//required by material-ui
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+
+class Main extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      widths: {
+        sidebar: 3,
+        main: 6,
+        preview: 3
+      },
+      curDocUuid: ''
+    }
+  }
+
+  componentWillMount() {
+
+    this.unsub = [];
+
+    // create blank doc
+    this.unsub.push(DocStore.createDoc().onValue(curDocUuid => this.setState({curDocUuid})))
+
+
+/*    this.unsub.push(
+      NoteStore.currentTagObservable.onValue(() =>
+
+          that.setState({
+            widths: {
+              sidebar: 3,
+              main: 6,
+              preview: 3
+            }
+          })
+      )
+    );
+
+    this.unsub.push(
+      NoteStore.currentDocObservable.onValue(() =>
+
+          that.setState({
+            widths: {
+              sidebar: 2,
+              main: 7,
+              preview: 3
+            }
+          })
+      )
+    );*/
+  }
+
+  componentWillUnmount() {
+
+    this.unsub.forEach(unsub => unsub());
+  }
+
+  render() {
+    return (
+      <div className='row foo'>
+        <div className='col-sm-9'>
+          <Editor uuid={this.state.curDocUuid} />
+        </div>
+        <div className='col-sm-3'>
+          <Preview uuid={this.state.curDocUuid} />
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Main/>,
+  document.getElementById('app')
+);
+
