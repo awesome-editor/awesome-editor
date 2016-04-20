a5360e55d85f46af4a83ed3e5a7a22dbe389b4c1
Uriel Avalos <uriel.avalos@drillinginfo.com>
2016-04-20 00:53:36 -0500

    [doc] cleanup

--- src/components/docs/DocEditor.js
+++ src/components/docs/DocEditor.js
@@ -1,6 +1,5 @@
 /*eslint no-extra-parens: 0*/
 import React from 'react'
-import _ from 'lodash'
 
 import Card from 'material-ui/lib/card/card'
 import CardText from 'material-ui/lib/card/card-text'
@@ -10,10 +9,8 @@ import TextField from 'material-ui/lib/text-field'
 import TagList from '../tags/TagList'
 import DocData from '../../stores/docs/DocData'
 import DivEdit from './../editor/DivEdit'
-import TagStore from '../../stores/tags/TagStore'
-import '../../util/Utils.js'
-import AppState from '../../app/AppState'
 
+const noop = () => undefined
 
 const Editor = ({previewMode, doc, tags, updateDoc, autocompleteTag, createTag, addTag}) => (
 
@@ -25,14 +22,14 @@ const Editor = ({previewMode, doc, tags, updateDoc, autocompleteTag, createTag, 
         multiLine={true}
         value={doc.title}
         fullWidth={true}
-        onChange={previewMode ? _.noop : evt => updateDoc({uuid: doc.uuid, title: evt.target.value})}
+        onChange={previewMode ? noop : evt => updateDoc({uuid: doc.uuid, title: evt.target.value})}
       />
       <TextField
         hintText="Subtitle"
         multiLine={true}
         value={doc.subtitle}
         fullWidth={true}
-        onChange={previewMode ? _.noop : evt => updateDoc({uuid: doc.uuid, subtitle: evt.target.value})}
+        onChange={previewMode ? noop : evt => updateDoc({uuid: doc.uuid, subtitle: evt.target.value})}
       />
     </CardText>
 
@@ -41,7 +38,7 @@ const Editor = ({previewMode, doc, tags, updateDoc, autocompleteTag, createTag, 
         key="Content"
         style={{minHeight: '3em'}}
         html={doc.content}
-        onChange={previewMode ? _.noop : evt => updateDoc({uuid: doc.uuid, content: evt.target.value})}>
+        onChange={previewMode ? noop : evt => updateDoc({uuid: doc.uuid, content: evt.target.value})}>
       </DivEdit>
     </CardText>
 
