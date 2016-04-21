0143fa69a3d72c40e8e93515d002cd2d682b36ad
Uriel Avalos <uriel.avalos@drillinginfo.com>
2016-04-20 21:45:41 -0500

    [rflux] container pretty much finished

--- src/app/Container.js
+++ src/app/Container.js
@@ -3,6 +3,14 @@ import React from 'react'
 import {isObservable} from '../util/Utils'
 
 
+/**
+ * In the props you pass normal properties and observables.
+ * The observables is where the magic is---for each observable, add a listener,
+ * and set the state of the Container equal to the observed value.
+ * For example, if props = {obs1: observable} and the observed values are a, b, c...,
+ * then this.state.obs1 = a, b, c, ...
+ * Container then passes this state to its child as props.
+ */
 export default class Container extends React.Component {
 
   constructor(props) {
@@ -20,9 +28,11 @@ export default class Container extends React.Component {
 
   componentWillMount() {
 
-    this.unsub = this.observables.map(
-        obj => obj.observable._onValue(val => this.setState({[obj.property]: val}))
-      ) || []
+    this.setState(this.props.children.type.defaultProps || {}, () =>
+      this.unsub = this.observables.map(
+          obj => obj.observable._onValue(val => this.setState({[obj.property]: val}))
+        ) || []
+    )
   }
 
   componentWillUnmount() {
