c3a30c27534d9e1a4dfa673cbed1208172f1e037
Uriel Avalos <uriel.avalos@drillinginfo.com>
2016-04-23 00:29:46 -0500

    [divedit] added selection

--- src/components/editor/DivEdit.js
+++ src/components/editor/DivEdit.js
@@ -1,13 +1,15 @@
 import React from 'react'
 
+import {saveSelection, restoreSelection} from './Selection'
+
 
 //thanks https://github.com/lovasoa/react-contenteditable/blob/master/src/react-contenteditable.js
 export default class DivEdit extends React.Component {
 
   constructor() {
 
-    super();
-    this.emitChange = this.emitChange.bind(this);
+    super()
+    this.emitChange = this.emitChange.bind(this)
   }
 
   render() {
@@ -15,42 +17,51 @@ export default class DivEdit extends React.Component {
     return (
       <div
         {...this.props}
-        ref={(e) => this.htmlEl = e}
+        key="DivEdit"
+        ref={e => this.htmlEl = e}
         onInput={this.emitChange}
         onBlur={this.emitChange}
         contentEditable={!this.props.disabled}
         dangerouslySetInnerHTML={{__html: this.props.html}}>
       </div>
-    );
+    )
   }
 
   shouldComponentUpdate(nextProps) {
-    return !this.htmlEl || nextProps.html !== this.htmlEl.innerText;
+
+    return !this.htmlEl || nextProps.html !== this.htmlEl.innerText
   }
 
   componentDidUpdate() {
+
     if ( this.htmlEl && this.props.html !== this.htmlEl.innerText ) {
-      this.htmlEl.innerText = this.props.html;
+
+      this.htmlEl.innerText = this.props.html
+      restoreSelection(this.selection)
+      this.htmlEl.focus()
     }
   }
 
   emitChange(evt) {
 
-    if (!this.htmlEl) { return; }
+    if (!this.htmlEl) { return }
 
-    var html = this.htmlEl.innerText;
+    const html = this.htmlEl.innerText
 
     if (this.props.onChange && html !== this.lastHtml) {
-      evt.target = { value: html };
-      this.props.onChange(evt);
+
+      evt.target = {value: html}
+      this.selection = saveSelection()
+
+      this.props.onChange(evt)
     }
 
-    this.lastHtml = html;
+    this.lastHtml = html
   }
 }
 
 DivEdit.props = {
-  onChange: () => {},
+  onChange: () => undefined,
   html: '',
   disabled: false
-};
+}
