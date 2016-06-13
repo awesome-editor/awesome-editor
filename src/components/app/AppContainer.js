ed761922499bd32e66eeac3a5d2b63abb8f58d04
Uriel Avalos <uriel.avalos@drillinginfo.com>
2016-06-12 22:36:45 -0500

    [refac] migrated to new containers

--- src/components/app/AppContainer.js
+++ src/components/app/AppContainer.js
@@ -5,13 +5,23 @@ import App from './App'
 
 
 export default createContainer({
+  getInitialState() {
+    return {
+      mainWindow: 'DocList',
+      currentDocUuid: null,
+      switchToEditor: false,
+      switchToList: true,
+      systemCreateDoc: () => undefined,
+      systemSwitchMainWindow: () => undefined
+    }
+  },
   getInitialObservableState() {
     return {
       mainWindow: AppState.appMainWindowObservable,
       currentDocUuid: AppState.appCurrentDocUuidObservable,
       switchToEditor: AppState.appMainWindowObservable.diff((prev, cur) => cur === 'DocEditor' && prev !== cur, false),
       switchToList: AppState.appMainWindowObservable.diff((prev, cur) => cur === 'DocList' && prev !== cur, false),
-      systemCreateDoc: AppState.systemCreateDoc,
+      //systemCreateDoc: AppState.systemCreateDoc, TODO FIX ME
       systemSwitchMainWindow: AppState.systemSwitchMainWindow
     }
   }
